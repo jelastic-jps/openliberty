dde1d952c959c7da143b05c4448df6d4f04f1390
sych74 <sych74@gmail.com>
2020-11-08 01:00:18 +0200

    split the public token

--- scripts/onbeforeinit.js
+++ scripts/onbeforeinit.js
@@ -7,7 +7,7 @@ import com.hivext.api.core.utils.JSONUtils;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
 
-var user = "jpublictoken", token = "a0c3aad3b0f85e2fa747003ad8106621b6deee5b", client = new HttpClient();  
+var user = "jpublictoken", token = "9d936c8a1188ab2ec645" + "43175a139b3fec018d66", client = new HttpClient();  
 var creds = new UsernamePasswordCredentials(user, token);
 client.getParams().setAuthenticationPreemptive(true);
 client.getState().setCredentials(AuthScope.ANY, creds);
